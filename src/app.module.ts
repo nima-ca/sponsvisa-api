c9c074c170c65254b81e53a980f5d239a6a132ca
nima <nekoueinima@protonmail.com>
2023-06-14 11:26:40 +0330

    [Feat]: add common, config, and jwt module and add nodemailer, bcrypt, joi, jsonwebtoken and nanoid packages and add ConfigModule to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from "@nestjs/common";
 import { AuthModule } from "./auth/auth.module";
+import { JwtModule } from "./jwt/jwt.module";
+import { ConfigModule } from "@nestjs/config";
+import { validationSchema } from "./config/config.schema";
+import { config } from "./config/config";
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema,
+      load: [config],
+    }),
+    AuthModule,
+    JwtModule,
+  ],
 })
 export class AppModule {}
