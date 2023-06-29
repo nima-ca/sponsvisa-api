513ad4a06091b9bb32b75d0d5bc5949ccb489838
nima <nekoueinima@protonmail.com>
2023-06-29 11:03:33 +0330

    [Test]: add unit test for mail service

--- src/mail/mail.service.spec.ts
+++ src/mail/mail.service.spec.ts
@@ -1,18 +1,62 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { MailService } from "./mail.service";
+import { ConfigService } from "@nestjs/config";
+import * as nodemailer from "nodemailer";
+
+const sendMailMock = jest.fn();
+jest.mock(`nodemailer`, () => ({
+  createTransport: jest.fn().mockImplementation(() => ({
+    sendMail: sendMailMock,
+  })),
+}));
 
 describe(`MailService`, () => {
   let service: MailService;
+  let configService: ConfigService;
+
+  const MOCKED_SENDER_EMAIL = `your-email@hotmail.com`;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MailService],
+      providers: [
+        MailService,
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn((key: string) => {
+              if (key === `EMAIL_HOST`) return `smtp-mail.outlook.com`;
+              if (key === `EMAIL_USER`) return MOCKED_SENDER_EMAIL;
+              if (key === `EMAIL_PASSWORD`) return `your-email-password`;
+            }),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<MailService>(MailService);
+    configService = module.get<ConfigService>(ConfigService);
   });
 
   it(`should be defined`, () => {
     expect(service).toBeDefined();
+    expect(configService).toBeDefined();
+  });
+
+  describe(`sendEmail`, () => {
+    it(`should send an email successfully`, async () => {
+      const to = `recipient@example.com`;
+      const subject = `Hello`;
+      const text = `This is a test email`;
+
+      await service.sendEmail(to, subject, text);
+
+      expect(sendMailMock).toHaveBeenCalledWith({
+        from: MOCKED_SENDER_EMAIL,
+        to,
+        subject,
+        text,
+      });
+      expect.hasAssertions();
+    });
   });
 });
